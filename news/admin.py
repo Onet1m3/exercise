42b2419ca049d0e9e06514a75ac4413e2992ef3f
Onet1m3 <hand2note@yahoo.com>
2021-04-07 22:57:18 +0300

    Админка для News

--- news/admin.py
+++ news/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import NewsModel
 
-# Register your models here.
+
+@admin.register(NewsModel)
+class NewsModelAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'publish_date')
+    list_filter = ('title', 'publish_date')
+    save_on_top = True
